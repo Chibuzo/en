de0ddf7d871a95a64ad4ce0740f847c9359c20fc
Chibuzo <chibuzo.henry@gmail.com>
2021-10-30 23:35:30 +0100

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const authenticate = require('../middlewares/authenticate');
+const userService = require('../services/userService');
+
+
+router.get('/', async (req, res, next) => {
+    try {
+        const investments = await investmentService.list();
+        res.render('index', { title: 'Welcome', investments });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup', { title: 'Sign Up' });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+router.post('/signup', async (req, res, next) => {
+    try {
+        const newUser = await userService.create(req.body);
+        res.render('signup', { title: 'Sign Up', newUser });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.post('/login', async (req, res, next) => {
+    try {
+        req.session.user = await userService.login(req.body);
+        res.redirect('/users/dashboard');
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+module.exports = router;
